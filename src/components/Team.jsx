965d14aec7b8117fa3d39af62f15a0044153b2d1
ambijames <emmaabison@gmail.com>
2022-01-05 23:00:52 +0100

    Update team members

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -14,7 +14,7 @@ export const Team = (props) => {
               <div key={`${d.name}-${i}`} className='col-md-3 col-sm-6 team'>
                 <div className='thumbnail'>
                   {' '}
-                  <img src={d.img} alt='...' className='team-img' />
+                  <img src={d.img} alt='...' className='team-img' style={{}}/>
                   <div className='caption'>
                     <h4>{d.name}</h4>
                     <p>{d.job}</p>
